bdadf161bc8db7988aa72a0559b03e43130db818
Yoshiki Ozaki <y.ozaki@exflora.jp>
2018-01-15 17:52:06 +0900

    example template

---
+++ templates/example/params.js
@@ -0,0 +1,8 @@
+module.exports = {
+  items: [
+    { name: 'Product A', amount: '2', price: '200' },
+    { name: 'Product B', amount: '1', price: '600' },
+  ],
+  note: "This is just a sample",
+  clientName: "MyClient inc."
+};
